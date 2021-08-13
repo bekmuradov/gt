b01bafcca7177f412eee4900d13cec9cecec22e1
Beck <47065940+bekmuradov@users.noreply.github.com>
2021-08-13 23:08:28 +0700

    added netlify form

--- dist/forms/contact.php
+++ dist/forms/contact.php
@@ -6,7 +6,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'getcyberpresence@gmail.com';
+  $receiving_email_address = '';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -26,9 +26,9 @@
   
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
-    'username' => 'getcyberpresence@gmail.com',
-    'password' => 'V4kZSBVAR745zdW',
-    'port' => '587'
+    'username' => '',
+    'password' => '',
+    'port' => ''
   );
 
   $contact->add_message( $_POST['name'], 'From');
